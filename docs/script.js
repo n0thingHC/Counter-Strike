ae39dc3e3b2494c789e637d0a7b6ccc7ccc78804
Davi J. <151475869+n0thingHC@users.noreply.github.com>
2024-11-28 23:55:25 +0000

    modifica

--- docs/script.js
+++ docs/script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
     const navMenu = document.getElementById("navMenu");
     navMenu.classList.toggle("show");
 }
+const toggleButton = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+// Verifica se o usuário tem preferência salva
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+    body.classList.add('dark-mode');
+    toggleButton.classList.add('dark-mode'); // Adiciona a classe ao botão
+}
+
+// Alterna entre os temas
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    toggleButton.classList.toggle('dark-mode'); // Alterna a classe do botão
+    const theme = body.classList.contains('dark-mode') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme); // Salva a preferência do usuário
+});
