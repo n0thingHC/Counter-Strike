29da0e2262f0e7ba889c4039ade02a4c67d0a915
ahiruvictor <202408167237@alunos.unimetrocamp.edu.br>
2024-11-28 23:51:27 -0300

    Mudanças

--- docs/script.js
+++ docs/script.js
@@ -1,23 +1,18 @@
-// script.js
-
-function toggleMenu() {
-    const navMenu = document.getElementById("navMenu");
-    navMenu.classList.toggle("show");
-}
+// Alternar entre os temas (Claro / Escuro)
 const toggleButton = document.getElementById('dark-mode-toggle');
 const body = document.body;
 
-// Verifica se o usuário tem preferência salva
+// Verifica se o usuário tem preferência de tema salva no localStorage
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme === 'dark') {
     body.classList.add('dark-mode');
-    toggleButton.classList.add('dark-mode'); // Adiciona a classe ao botão
+    toggleButton.classList.add('dark-mode'); // Adiciona a classe ao botão para refletir o estado do tema
 }
 
-// Alterna entre os temas
+// Alterna o tema entre claro e escuro
 toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    toggleButton.classList.toggle('dark-mode'); // Alterna a classe do botão
+    body.classList.toggle('dark-mode'); // Alterna a classe no body
+    toggleButton.classList.toggle('dark-mode'); // Alterna a classe no botão
     const theme = body.classList.contains('dark-mode') ? 'dark' : 'light';
-    localStorage.setItem('theme', theme); // Salva a preferência do usuário
+    localStorage.setItem('theme', theme); // Salva a preferência do usuário no localStorage
 });
